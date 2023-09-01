146243eac7619f06de52f54f98fda5602dc6ae86
roddylan <51688965+roddylan@users.noreply.github.com>
2023-09-01 14:23:25 -0600

    Fixed board representation in pbitboard.cpp + QoL improvements

--- src/pbitboard.cpp
+++ src/pbitboard.cpp
@@ -1,6 +1,11 @@
-// bitboard.cpp
-#include "bitboard.hpp"
-
+// pbitboard.cpp
+// for pieces
+#include "pbitboard.hpp"
+#ifndef _U64
+#include <cstdint>
+typedef std::uint64_t U64;
+#define _U64
+#endif
 // bitboard::starting {
 //     {"wp", 65280},
 //     {"bp", 71776119061217280},
@@ -23,51 +28,74 @@ pBitboard::pBitboard() {
 
 void pBitboard::setup(std::string FEN) {
     // "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-    int i = 0;
-    std::bitset<64> currentPiece(0x8000000000000000);
+    // int i = 0;
+    // std::bitset<64> currentPiece(0x8000000000000000);
+
+
     
+    int i = 0;
+    // little endian
+    // 0000 0001 A
+    // 0000 0010 B ...
+    // 1000 0000 H
+
+    // 0x00000000000000FF -> 1 RANK
+    // 0xFF00000000000000 -> 8 RANK
+    // LSF
+    
+    std::bitset<64> firstPiece(0x0000000000000001);
+    
+    int rankIdx = 7;
+    int fileIdx = 0;
 
     while (FEN[i] && FEN[i] != ' ') {
 
         if (FEN[i] >= 48 && FEN[i] <= 57) {
             int temp = (FEN[i] - '0');
-            currentPiece >>= temp;
+            fileIdx += temp;
+            // currentPiece <<= temp;
         }
         else {
             if (FEN[i] == '/') {
+                rankIdx -= 1;
+                fileIdx = 0;
                 i++;
-                continue;
+                // continue;
             }
             else {
-                *pieces[FEN[i]] ^= currentPiece;
-                currentPiece >>= 1;
+                int squareIdx = 8*rankIdx + fileIdx;
+                *m_pieceMap[FEN[i]] |= (firstPiece << squareIdx).to_ullong();
+                fileIdx += 1;
+                // currentPiece <<= 1;
                 i++;
             }
         }
-        int field = 0;
-        for (i; i < FEN.length(); i++) {
-            if (FEN[i] == ' ') {
-                field++;
-                continue;
-            }
-            if (field == 1) {
-                // active color
-                activeColor = FEN[i] % 2;
-            }
-            else if (field == 2) {
-                // casting rights
-                // upper case first (white)
-                // "k" -> kingside castling; "q" -> queenside castling; "-" no castling
-                // lower case after (black)
-            }
-            else if (field == 3) {
-                // possible en passant targets
-                
-            }
-
+    }
+    
+    int field = 0;
+    for (i; i < FEN.length(); i++) {
+        if (FEN[i] == ' ') {
+            field++;
+            continue;
         }
+        if (field == 1) {
+            // active color
+            m_activeColor = ((int) FEN[i]) % 2;
+        }
+        else if (field == 2) {
+            // casting rights
+            // upper case first (white)
+            // "k" -> kingside castling; "q" -> queenside castling; "-" no castling
+            // lower case after (black)
+        }
+        else if (field == 3) {
+            // possible en passant targets
+            
+        }
+
     }
 
-    // (length - i) to (length - 1)
+    
+    
     return;
 }
